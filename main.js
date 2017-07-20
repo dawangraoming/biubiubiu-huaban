99b60c7f3e872ed59c35e847ff826a71e79fa05a
大王饶命 <514266254@qq.com>
2017-07-20 20:45:24 +0800

    init

---
+++ main.js
@@ -0,0 +1,60 @@
+/*!
+ * @author xueyangchu
+ * @date 2017/7/18
+ */
+"use strict";
+
+
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require("path");
+const url = require('url');
+
+const server = require('./app/server');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
+
+
+ipcMain.on('start', function (e, arg) {
+    let result = server.start(arg.path);
+    e.sender.send('start-callback', result);
+});
+
+ipcMain.on('stop', function (e, arg) {
+    let result = server.stop();
+    e.sender.send('stop-callback', result);
+});
+
+ipcMain.on('setPath', function (e, arg) {
+
+});
+
